d0170044203a92d3c72a3eb439c8910ff22fca5f
Satendrajio <162817731+Satendrajio@users.noreply.github.com>
2024-10-25 11:50:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -261,7 +261,7 @@ async def start(client, message):
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                     ],[
-                        InlineKeyboardButton('🚀Ultra Fast Stream/Download Mode🚀', url="https://t.me/Filetolinkcrown_bot")
+                        InlineKeyboardButton('👨CONNTACT ADMIN👨', url="https://t.me/OneSupportSystem_bot")
                     ],[
                         InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
                         InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)
@@ -271,7 +271,7 @@ async def start(client, message):
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                     ],[
-                        InlineKeyboardButton('🚀Ultra Fast Stream/Download Mode🚀', url="https://t.me/Filetolinkcrown_bot")
+                        InlineKeyboardButton('👨CONNTACT ADMIN👨', url="https://t.me/OneSupportSystem_bot")
                     ]]
                 msg = await client.send_cached_media(
                     chat_id=message.from_user.id,
@@ -293,7 +293,7 @@ async def start(client, message):
                     reply_markup=InlineKeyboardMarkup(button)
                 )
                 filesarr.append(msg)
-                k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to Ultra Fast Download/Stream Bot for better downloading experience and Start Download there</i></b>.\n\n<b><i>👉इस बोट से प्राप्त फाइल/विडियो को कृप्या Ultra Fast Download/Stream Mode Bot में फॉरवर्ड करें बेहतर डाउनलोडिंग अनुभव एवं ऑनलाइन देखने के लिए।</i></b>")
+                k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to your Saved Masseges and Start Download there</i></b>.\n\n<b><i>👉 कृपया इस बोट से प्राप्त फाइल/विडियो को किसी सुरक्षित जगह फॉरवर्ड करें एवं वहां डाऊनलोड करे।</i></b>")
                 await asyncio.sleep(600)
                 for x in filesarr:
                     await x.delete()
@@ -304,7 +304,7 @@ async def start(client, message):
                 continue
             await asyncio.sleep(1) 
         await sts.delete()
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to Ultra Fast Download/Stream Bot for better downloading experience and Start Download there</i></b>.\n\n<b><i>👉इस बोट से प्राप्त फाइल/विडियो को कृप्या Ultra Fast Download/Stream Mode Bot में फॉरवर्ड करें बेहतर डाउनलोडिंग अनुभव एवं ऑनलाइन देखने के लिए।</i></b>")
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to your Saved Masseges and Start Download there</i></b>.\n\n<b><i>👉 कृपया इस बोट से प्राप्त फाइल/विडियो को किसी सुरक्षित जगह फॉरवर्ड करें एवं वहां डाऊनलोड करे।</i></b>")
         await asyncio.sleep(600)
         for x in filesarr:
             await x.delete()
@@ -456,7 +456,7 @@ async def start(client, message):
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
                     InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                 ],[
-                    InlineKeyboardButton("🚀Ultra Fast Stream/Download Mode🚀", url="https://t.me/Filetolinkcrown_bot")
+                    InlineKeyboardButton("👨CONTACT ADMIN👨", url="https://t.me/OneSupportSystem_bot")
                 ],[
                     InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @KingVJ01
                 ]]
@@ -465,7 +465,7 @@ async def start(client, message):
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
                     InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                 ],[
-                    InlineKeyboardButton("🚀Ultra Fast Stream/Download Mode🚀", url="https://t.me/Filetolinkcrown_bot")
+                    InlineKeyboardButton("👨 CONTACT ADMIN 👨", url="https://t.me/OneSupportSystem_bot")
                 ]]
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
@@ -475,7 +475,7 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(button)
             )
             filesarr.append(msg)
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to Ultra Fast Download/Stream Bot for better downloading experience and Start Download there</i></b>.\n\n<b><i>👉इस बोट से प्राप्त फाइल/विडियो को कृप्या Ultra Fast Download/Stream Mode Bot में फॉरवर्ड करें बेहतर डाउनलोडिंग अनुभव एवं ऑनलाइन देखने के लिए।</i></b>")
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie Files/Videos will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to your Saved Masseges and Start Download there</i></b>.\n\n<b><i>👉 कृपया इस बोट से प्राप्त फाइल/विडियो को किसी सुरक्षित जगह फॉरवर्ड करें एवं वहां डाऊनलोड करे।</i></b>")
         await asyncio.sleep(600)
         for x in filesarr:
             await x.delete()
@@ -529,7 +529,7 @@ async def start(client, message):
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
                     InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                 ],[
-                    InlineKeyboardButton("🚀Ultra Fast Stream/Download Mode🚀", url="https://t.me/Filetolinkcrown_bot")
+                    InlineKeyboardButton("👨CONTACT ADMIN 👨 ", url="https://t.me/OneSupportSystem_bot")
                 ],[
                     InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @KingVJ01
                 ]]
@@ -538,7 +538,7 @@ async def start(client, message):
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
                     InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                 ],[
-                    InlineKeyboardButton("🚀Ultra Fast Stream/Download Mode🚀", url="https://t.me/Filetolinkcrown_bot")
+                    InlineKeyboardButton("👨CONTACT ADMIN 👨", url="https://t.me/OneSupportSystem_bot")
                 ]]
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
@@ -563,7 +563,7 @@ async def start(client, message):
             btn = [[
                 InlineKeyboardButton("Get File Again", callback_data=f'delfile#{file_id}')
             ]]
-            k = await msg.reply("<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to Ultra Fast Download/Stream Bot for better downloading experience and Start Download there</i></b>.\n\n<b><i>👉इस बोट से प्राप्त फाइल/विडियो को कृप्या Ultra Fast Download/Stream Mode Bot में फॉरवर्ड करें बेहतर डाउनलोडिंग अनुभव एवं ऑनलाइन देखने के लिए।</i></b>")
+            k = await msg.reply("<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to your Saved Masseges and Start Download there</i></b>.\n\n<b><i>👉 कृपया इस बोट से प्राप्त फाइल/विडियो को किसी सुरक्षित जगह फॉरवर्ड करें एवं वहां डाऊनलोड करे।</i></b>")
             await asyncio.sleep(600)
             await msg.delete()
             await k.edit_text("<b>Your File/Video is successfully deleted!!!\n\nClick below button to get your deleted file 👇</b>",reply_markup=InlineKeyboardMarkup(btn))
@@ -601,7 +601,7 @@ async def start(client, message):
             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
         ],[
-            InlineKeyboardButton("🚀Ultra Fast Stream/Download Mode🚀", url="https://t.me/Filetolinkcrown_bot")
+            InlineKeyboardButton("👨CONTACT ADMIN 👨", url="https://t.me/OneSupportSystem_bot")
         ],[
             InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @KingVJ01
         ]]
@@ -610,7 +610,7 @@ async def start(client, message):
             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
         ],[
-            InlineKeyboardButton("🚀Ultra Fast Stream/Download Mode🚀", url="https://t.me/Filetolinkcrown_bot")
+            InlineKeyboardButton("👨CONTACT ADMIN 👨", url="https://t.me/OneSupportSystem_bot")
         ]]
     msg = await client.send_cached_media(
         chat_id=message.from_user.id,
@@ -622,7 +622,7 @@ async def start(client, message):
     btn = [[
         InlineKeyboardButton("Get File Again", callback_data=f'delfile#{file_id}')
     ]]
-    k = await msg.reply("<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to Ultra Fast Download/Stream Bot for better downloading experience and Start Download there</i></b>.\n\n<b><i>👉इस बोट से प्राप्त फाइल/विडियो को कृप्या Ultra Fast Download/Stream Mode Bot में फॉरवर्ड करें बेहतर डाउनलोडिंग अनुभव एवं ऑनलाइन देखने के लिए।</i></b>")
+    k = await msg.reply("<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>👉Please forward this ALL Files/Videos to your Saved Masseges and Start Download there</i></b>.\n\n<b><i>👉 कृपया इस बोट से प्राप्त फाइल/विडियो को किसी सुरक्षित जगह फॉरवर्ड करें एवं वहां डाऊनलोड करे।</i></b>")
     await asyncio.sleep(600)
     await msg.delete()
     await k.edit_text("<b>Your File/Video is successfully deleted!!!\n\nClick below button to get your deleted file 👇</b>",reply_markup=InlineKeyboardMarkup(btn))
